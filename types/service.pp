b60a026bb5d6c40c667e78660fb315a45d4f7305
Matteo Cerutti <matteo.cerutti@rkrepsilon.com>
2017-08-08 15:46:40 +0100

    allowing dst_port and src_port to be strings.

--- types/service.pp
+++ types/service.pp
@@ -1,6 +1,6 @@
 type Fluffy::Service = Struct[{
-  Optional[src_port] => Array[Variant[Pattern[/^\d+:\d+$/], Integer[1, 65535]]],
-  Optional[dst_port] => Array[Variant[Pattern[/^\d+:\d+$/], Integer[1, 65535]]],
-  Optional[protocol] => Enum['ip', 'tcp', 'udp', 'icmp', 'ipv6-icmp', 'esp', 'ah', 'vrrp', 'igmp', 'ipencap', 'ipv4', 'ipv6', 'ospf', 'gre', 'cbt', 'sctp', 'pim', 'any'],
+  Optional[src_port] => Variant[Fluffy::Service::Port, Array[Fluffy::Service::Port]],
+  Optional[dst_port] => Variant[Fluffy::Service::Port, Array[Fluffy::Service::Port]],
+  Optional[protocol] => Fluffy::Protocols,
   Optional[ensure] => Enum["present", "absent"]
 }]
