cce4e10237790d24f1a494dd877e9b07e25bcb2e
Matteo Cerutti <matteo.cerutti@rkrepsilon.com>
2017-08-08 14:40:07 +0100

    adding more icmp types rule protocol can now take an array

--- lib/puppet/type/fluffy_rule.rb
+++ lib/puppet/type/fluffy_rule.rb
@@ -68,7 +68,7 @@ Puppet::Type.newtype(:fluffy_rule) do
     desc 'ICMP type'
 
     defaultto(:absent)
-    newvalues(:absent,:any,'echo-reply','echo-request')
+    newvalues(:absent, :any, 'echo-reply','destination-unreachable','network-unreachable','host-unreachable','protocol-unreachable','port-unreachable','fragmentation-needed','source-route-failed','network-unknown','host-unknown','network-prohibited','host-prohibited','TOS-network-unreachable','TOS-host-unreachable','communication-prohibited','host-precedence-violation','precedence-cutoff','source-quench','redirect','network-redirect','host-redirect','TOS-network-redirect','TOS-host-redirect','echo-request','router-advertisement','router-solicitation','time-exceeded','ttl-zero-during-transit','ttl-zero-during-reassembly','parameter-problem','ip-header-bad','required-option-missing','timestamp-request','timestamp-reply','address-mask-request','address-mask-reply')
   end
 
   newproperty(:negate_tcp_flags, :boolean => true) do
---
+++ types/protocols.pp
@@ -0,0 +1,1 @@
+type Fluffy::Protocols = Enum['ip', 'tcp', 'udp', 'icmp', 'ipv6-icmp', 'esp', 'ah', 'vrrp', 'igmp', 'ipencap', 'ipv4', 'ipv6', 'ospf', 'gre', 'cbt', 'sctp', 'pim', 'any']
